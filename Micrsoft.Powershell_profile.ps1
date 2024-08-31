1ae6dd46523e9d26e3e11e90d1b97ba13ef5d094
keanehatescoding <keanembae@gmail.com>
2024-08-31 22:05:32 +0300

    Test

--- Micrsoft.Powershell_profile.ps1
+++ Micrsoft.Powershell_profile.ps1
@@ -2,4 +2,5 @@ oh-my-posh.exe init pwsh --config $env:POSH_THEMES_PATH/neko.omp.json | Invoke-E
 Invoke-Expression (& {(zoxide.exe init powershell | Out-String)})
 Set-Alias -Name cd -Value z -Option AllScope
 Set-Alias -Name v -Value nvim
+Set-Alias -Name bat -Value cat
 
